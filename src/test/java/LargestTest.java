007ccd3408eb0b8d17f3a2026c3c695c9768a176
JinTaeHo <wlsxogh3@gmail.com>
2020-06-12 22:53:18 +0900

    travis test

---
+++ src/test/java/LargestTest.java
@@ -0,0 +1,41 @@
+/**
+ * @author Taeho
+ * @since 2020-06-12, 오후 10:44
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LargestTest {
+
+    @Test
+    public void test1() {
+        assertEquals(9, Largest.largest(new int[]{7, 8, 9}));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(9, Largest.largest(new int[]{9, 8, 7}));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(20, Largest.largest(new int[]{8, 20, 7}));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(9, Largest.largest(new int[]{7, 8, 9}));
+    }
+
+    @Test
+    public void testEmpty() {
+        try {
+            Largest.largest(new int[]{});
+            fail("Should have thrown an exception");
+        } catch (RuntimeException e) {
+            assertTrue(true);
+        }
+    }
+}
